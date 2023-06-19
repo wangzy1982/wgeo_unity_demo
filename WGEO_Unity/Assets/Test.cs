d2fb84ffcce8c8e963095bc1a5be415045f54957
wangzuoy <wangzy@bocspace.cn>
2023-06-19 22:46:38 +0800

    修改测试

--- WGEO_Unity/Assets/Test.cs
+++ WGEO_Unity/Assets/Test.cs
@@ -7,9 +7,9 @@ public class Test : MonoBehaviour
 {
     void Start()
     {
-        var surface = wgeo.NewSphere(new wgeo.Vector3(0, 0, 0), 1, new wgeo.Interval(0, Math.PI), new wgeo.Interval(0, Math.PI));
+        var surface = wgeo.NewSphere(new wgeo.Vector3(0, 0, 0), 1, new wgeo.Interval(0, Math.PI), new wgeo.Interval(-Math.PI, Math.PI));
         var surfaceFeature = wgeo.NewSurfaceFeature(surface);
-        var mesh = wgeo.SurfaceToUnityMesh(surfaceFeature, true, true, Math.PI / 7);
+        var mesh = wgeo.SurfaceToUnityMesh(surfaceFeature, true, true, Math.PI / 6);
         wgeo.FreeSurfaceFeature(surfaceFeature);
         wgeo.FreeSurface(surface);
         var meshFilter = gameObject.GetComponent<MeshFilter>();
